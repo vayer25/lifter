27c6c81643f92ceab74e354a4c0dd972a9ccb0c4
vayer25 <yerlingcordero25@gmail.com>
2025-06-16 18:23:06 -0600

    Add students [] in the function call main as provided in the feedback

--- 10_Week/main.py
+++ 10_Week/main.py
@@ -6,28 +6,32 @@ from actions import (
 )
 from data import export_to_csv, import_from_csv
 
-students = []
+def main():
+    students = []
 
-while True:
-    show_menu()
-    option = input("Choose an option (0-7): ").strip()
+    while True:
+        show_menu()
+        option = input("Choose an option (0-7): ").strip()
 
-    if option == "1":
-        add_student(students)
-    elif option == "2":
-        view_all_students(students)
-    elif option == "3":
-        view_top_3_students(students)
-    elif option == "4":
-        calculate_overall_average(students)
-    elif option == "5":
-        export_to_csv(students)
-    elif option == "6":
-        students = import_from_csv()
-    elif option == "7":
-        show_top_student_each_subject(students)
-    elif option == "0":
-        print("🙌 Thanks for using the tool!")
-        break
-    else:
-        print("👎 Invalid option. Please enter a number between 0 and 7.")
+        if option == "1":
+            add_student(students)
+        elif option == "2":
+            view_all_students(students)
+        elif option == "3":
+            view_top_3_students(students)
+        elif option == "4":
+            calculate_overall_average(students)
+        elif option == "5":
+            export_to_csv(students)
+        elif option == "6":
+            students = import_from_csv()
+        elif option == "7":
+            show_top_student_each_subject(students)
+        elif option == "0":
+            print("🙌 Thanks for using the tool!")
+            break
+        else:
+            print("👎 Invalid option.")
+
+if __name__ == "__main__":
+    main()
